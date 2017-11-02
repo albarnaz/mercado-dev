e87a49bbff77c3a68cbe226bf7d80c490cfffb58
Wescley Matos <wescleymatos@gmail.com>
2017-11-02 20:07:58 -0300

    Criação do componente de anúncios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 import HeaderHome from './components/HeaderHome';
+import AnuncioHome from './components/AnuncioHome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HeaderHome />
+        <div className="container">
+          <AnuncioHome />
+          <AnuncioHome />
+          <AnuncioHome />
+        </div>
       </div>
     );
   }
