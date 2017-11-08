a15e4462bc950f58a57cfa10eadff6dfcaf7f7c2
Wescley Matos <wescleymatos@gmail.com>
2017-11-07 21:35:41 -0300

    Add rotemento de paginas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path='/' exact component={ Home } />
+        </div>
+      </Router>
     );
   }
 }
