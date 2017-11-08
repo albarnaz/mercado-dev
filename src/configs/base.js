6dd34a794627905da4594ced4931d22a7c9ed909
Wescley Matos <wescleymatos@gmail.com>
2017-11-07 23:00:49 -0300

    implementação do upload de arquivos

--- src/configs/base.js
+++ src/configs/base.js
@@ -1,17 +1,19 @@
 const config = {
-  apiKey: "AIzaSyAx0tU_s3cSliixSof27wrsHSUi3yPwoaQ",
-  authDomain: "mercado-dev.firebaseapp.com",
-  databaseURL: "https://mercado-dev.firebaseio.com",
-  projectId: "mercado-dev",
-  storageBucket: "mercado-dev.appspot.com",
-  messagingSenderId: "1006232735302"
+  apiKey: 'AIzaSyAx0tU_s3cSliixSof27wrsHSUi3yPwoaQ',
+  authDomain: 'mercado-dev.firebaseapp.com',
+  databaseURL: 'https://mercado-dev.firebaseio.com',
+  projectId: 'mercado-dev',
+  storageBucket: 'mercado-dev.appspot.com',
+  messagingSenderId: '1006232735302'
 };
 
 const Rebase = require('re-base');
 const firebase = require('firebase/app');
 require('firebase/database');
+require('firebase/storage');
 
 const app = firebase.initializeApp(config);
 const base = Rebase.createClass(app.database());
 
+export const storage = app.storage();
 export default base;
--- src/pages/NovoAnuncio.js
+++ src/pages/NovoAnuncio.js
@@ -2,36 +2,74 @@ import React, { Component } from 'react';
 
 import Footer from './../components/Footer';
 import HeaderInterno from './../components/HeaderInterno';
+import base, { storage } from './../configs/base';
 
 class NovoAnuncio extends Component {
+  constructor(props) {
+    super(props);
+
+    this.enviarAnuncio = this.enviarAnuncio.bind(this);
+  }
+
+  enviarAnuncio(event) {
+    event.preventDefault();
+
+    const file = this.foto.files[0];
+    const { name } = file;
+    const ref = storage.ref(name);
+
+    ref
+      .put(file)
+      .then(img => {
+        const anuncio = {
+          nome: this.nome.value,
+          foto: img.metadata.downloadURLs[0],
+          descricao: this.descricao.value,
+          preco: this.preco.value,
+          telefone: this.telefone.value,
+          vendedor: this.vendedor.value
+        };
+
+        base.push('anuncios', {
+          data: anuncio
+        }, (error) => {
+          console.log(error);
+        });
+      });
+  }
+
   render() {
     return (
       <div>
         <HeaderInterno />
         <div className='container' style={{ paddingTop: '120px' }}>
           <h3>Novo Anúncio</h3>
-          <form onSubmit={null}>
+          <form onSubmit={ this.enviarAnuncio }>
+          <div className='form-group'>
+            <label>Foto</label>
+            <input type='file' className='form-control' ref={ (ref) => this.foto = ref } />
+          </div>
             <div className='form-group'>
               <label>Nome</label>
-              <input type='text' className='form-control' placeholder='Nome' />
+              <input type='text' className='form-control' placeholder='Nome' ref={ (ref) => this.nome = ref } />
             </div>
             <div className='form-group'>
               <label>Descrição</label>
-              <input type='text' className='form-control' placeholder='Descrição' />
+              <input type='text' className='form-control' placeholder='Descrição' ref={ (ref) => this.descricao = ref } />
             </div>
             <div className='form-group'>
               <label>Preço</label>
-              <input type='text' className='form-control' placeholder='Preço' />
+              <input type='text' className='form-control' placeholder='Preço' ref={ (ref) => this.preco = ref } />
             </div>
             <div className='form-group'>
               <label>Telefone</label>
-              <input type='text' className='form-control' placeholder='Telefone' />
+              <input type='text' className='form-control' placeholder='Telefone' ref={ (ref) => this.telefone = ref } />
             </div>
             <div className='form-group'>
               <label>Vendedor</label>
-              <input type='text' className='form-control' placeholder='Vendedor' />
+              <input type='text' className='form-control' placeholder='Vendedor' ref={ (ref) => this.vendedor = ref } />
             </div>
-            <button type='sumit' className='btn btn-primary'>Salvar</button>
+            <button type='submit' className='btn btn-primary'>Salvar</button>
           </form>
         </div>
         <Footer />
